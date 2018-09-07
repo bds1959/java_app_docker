1401415188dc56ce836589cd641a203bdee18dbb
root <root@localhost.localdomain>
2018-09-06 22:43:40 -0400

    first commit

---
+++ DockerConnectMySQL.java
@@ -0,0 +1,53 @@
+
+import java.sql.*;
+
+public class DockerConnectMySQL {
+   static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   static final String DB_URL = "jdbc:mysql://172.17.0.2:3306/rubbos";
+
+   static final String USER = "root";
+   static final String PASS = "123456";
+   
+   public static void main(String[] args) {
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      Class.forName("com.mysql.jdbc.Driver");
+
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+      stmt = conn.createStatement();
+      String sql;
+      sql = "SELECT id, name FROM categories";
+      ResultSet rs = stmt.executeQuery(sql);
+
+      while(rs.next()){
+         int id  = rs.getInt("id");
+         String name = rs.getString("name");
+
+         System.out.println("ID: " + id);
+         System.out.println(", name: " + name);
+      }
+      rs.close();
+      stmt.close();
+      conn.close();
+   }catch(SQLException se){
+      se.printStackTrace();
+   }catch(Exception e){
+      e.printStackTrace();
+   }finally{
+      try{
+         if(stmt!=null)
+            stmt.close();
+      }catch(SQLException se2){
+      }
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }
+   }
+ }
+}
